6cfa4bdc39a98fa6b14e974b2b94de48c475734f
IvanGorbunov <ivan_0110@mail.ru>
2022-11-20 15:40:37 +0300

    added swagger

--- itfinder/urls.py
+++ itfinder/urls.py
@@ -1,7 +1,13 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from .yasg import urlpatterns as doc_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    # path('', include('projects.urls')),
+] \
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+              + doc_url
